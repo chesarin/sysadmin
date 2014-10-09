217df6a2280c1ede8255e07d0929d9c54201d394
Antonio Cesar Vargas <vargasces@gmail.com>
2014-10-08 22:40:52 -0400

    testing code

--- bash-script/ssh-setup.sh
+++ bash-script/ssh-setup.sh
@@ -5,7 +5,7 @@ setup()
     su - ${user}
     echo "Creating Keys for user $(whoami)"
     echo ${HOME}
-    rm -f ${HOME}.ssh/id_rsa ${HOME}.ssh/id_rsa.pub ${HOME}/.ssh/known_hosts
+    rm -f ${HOME}/.ssh/id_rsa ${HOME}/.ssh/id_rsa.pub ${HOME}/.ssh/known_hosts
     ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
 }
 
