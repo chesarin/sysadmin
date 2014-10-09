984e045490c51a02770239cefefd3846474441a0
Antonio Cesar Vargas <vargasces@gmail.com>
2014-10-08 22:37:08 -0400

    ssh setup

--- bash-script/ssh-setup.sh
+++ bash-script/ssh-setup.sh
@@ -1,8 +1,12 @@
 #/usr/bin/env bash
 setup()
 {
-    su - app
+    user=$1
+    su - ${user}
+    echo "Creating Keys for user $(whoami)"
     echo ${HOME}
+    rm -f ${HOME}.ssh/id_rsa ${HOME}.ssh/id_rsa.pub ${HOME}/.ssh/known_hosts
+    ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
 }
 
-setup
+setup app
